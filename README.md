0257349c224a0d175da1562fb46c24490cd4497e
Robert Bauer <robertsadresse@gmx.de>
2017-03-21 18:39:50 +0100

    Change Plotting Enginge to Plots

--- README.md
+++ README.md
@@ -1,8 +1,29 @@
 # brainvisionloader
 Loads raw brainvision data into Julia
 
-The magic happens in 
+The magic happens in
 
 readBrainVision.jl
 
-Check out script.jl to see how to load the exemplary datase and visualize it with Gadfly.
+Check out script.jl to see how to read & plot the exemplary dataset
+
+```julia
+filename = "./data/example"
+EEG, DataInfo, ChanInfo = readEEG(filename)
+
+
+y = EEG[25,1:10*get(DataInfo,"SamplingRate",1000)];
+x = 1:length(y);
+
+plot(x, y,
+    linewidth=2,
+    alpha=0.6,
+    title="EEG Trace",
+    legend=:none,
+    xlabel = "Time in ms",
+    ylabel = "Amplitude in μV",
+    size = (1024,480),
+    );
+```
+
+<img src = "./data/example.png">
--- script.jl
+++ script.jl
@@ -1,17 +1,26 @@
 # workspace()
+using Plots
+plotlyjs()
+# unicodeplots()
 include("readBrainVision.jl")
+
 filename = "./data/example"
 # DataInfo, ChanInfo = readHDR(filename)
 EEG, DataInfo, ChanInfo = readEEG(filename)
 
+
 ## --------
-using Gadfly
-y = EEG[21,1:10*get(DataInfo,"SamplingRate",1000)];
+y = EEG[25,1:10*get(DataInfo,"SamplingRate",1000)];
 x = 1:length(y);
-xticks = [0:get(DataInfo,"SamplingRate",1000):length(y);]
-plot(x=x,y=y,
-    Geom.line,
-    Guide.xlabel("Time (in ms)"),
-    Guide.ylabel("Amplitude (in \muV )"),
-    Guide.xticks(ticks=xticks)
-    )
+
+gui()
+plt = plot(x, y,
+    linewidth=2,
+    alpha=0.6,
+    title="EEG Trace",
+    legend=:none,
+    xlabel = "Time in ms",
+    ylabel = "Amplitude in μV",
+    size = (1024,480),
+    );
+savefig("./data/example.png")
