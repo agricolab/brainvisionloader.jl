1d38b4fc647e9b0884212645eb47a1e754ad1906
Benedikt Ehinger <behinger@uos.de>
2020-03-04 08:31:11 +0100

    julia 1.3

---
+++ docs/example.jl
@@ -0,0 +1,25 @@
+
+using Plots
+using ..brainvisionloader
+
+filename = "../data/example"
+EEG = brainvisionloader.loadFile(filename)
+
+## --------
+toi     = EEG.markers[2,2]-EEG.srate:EEG.markers[2,2]+EEG.srate;
+y       = EEG.data[25,toi]
+x       = [1:length(y)]-EEG.srate
+xtick   = -EEG.srate:(EEG.srate/4):EEG.srate
+
+plot(x, y,
+    linewidth=2,
+    alpha=0.6,
+    title="EEG Trace",
+    legend=:none,
+    xlabel = "Time [ms]",
+    ylabel = "Amplitude [μV]",
+    size = (1024,480),
+    xticks = xtick,
+    );
+gui()
+savefig("./example.png")
