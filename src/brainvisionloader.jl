885b35dd05af0aa7ccf6c234c95c9b8c75ee960f
Benedikt Ehinger <behinger@uos.de>
2020-03-04 08:25:11 +0100

    Files generated by PkgTemplates

---
+++ src/brainvisionloader.jl
@@ -0,0 +1,5 @@
+module brainvisionloader
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using brainvisionloader
+using Test
+
+@testset "brainvisionloader.jl" begin
+    # Write your own tests here.
+end
